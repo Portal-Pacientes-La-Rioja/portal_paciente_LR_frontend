a989ec30b94b1f6536c2e779ed7df2945e3cbe40
vpugliese <vpugliese@pall.cl>
2023-05-05 17:04:02 -0300

    Update: nuevo formato modal

--- src/components/ImgRotate/ImgRotate.jsx
+++ src/components/ImgRotate/ImgRotate.jsx
@@ -2,11 +2,12 @@ import { useState } from "react";
 import notImg from '../../assets/statics/no-image-found.png';
 import * as MdIcon from 'react-icons/md';
 import { environment } from "../../environments/environments.demo";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ImgRotate = ({ img }) => {
 
     const baseUrl = environment.baseURL
-    const nImg = notImg 
+    const nImg = notImg
     //rotate img
     const [rotate, setRotate] = useState(false);
     const [deg, setDeg] = useState(0);
@@ -20,15 +21,33 @@ const ImgRotate = ({ img }) => {
     }
 
     return (
-        <div className="mb-3 d-flex flex-column align-items-center">
-        <div className="d-flex w-100 justify-content-end">
-                <a  role="button" className='text-secondary btn-icon' onClick={(e) => rotateLeft()}><MdIcon.MdRotateLeft /></a>
-                <a  role="button" className='text-secondary btn-icon' onClick={(e) => rotateRigth()}><MdIcon.MdRotateRight /></a>
-        </div>
-            <div className="admin-patient__img-container">
-                <img className={`admin-patient__img ${rotate ? 'rotate-img' : ''}`} style={{ transform: `rotate(${deg}deg)` }} src={img ? img : nImg } alt={`document patient`} />
-            </div>
-        </div>
+        <Container className="mb-3 d-flex flex-column align-items-center">
+            <Row>
+                <Col>
+                    <div className="admin-patient__img-container">
+                        <img className={`admin-patient__img ${rotate ? 'rotate-img' : ''}`} style={{ transform: `rotate(${deg}deg)` }} src={img ? img : nImg} alt={`document patient`} />
+                    </div>
+                </Col>
+                <Col className="d-flex">
+                    <div className="my-tooltip" style={{height: 'fit-content'}}>
+                        <button type="button" className='btn text-secondary btn-icon' onClick={(e) => rotateLeft()}><MdIcon.MdRotateLeft className="fs-3" /></button>
+                        <span className="tiptext">
+                            Girar a la izquierda
+                        </span>
+                    </div>
+                    <div className="my-tooltip" style={{height: 'fit-content'}}>
+                        <button type="button" className='btn text-secondary btn-icon' onClick={(e) => rotateRigth()}><MdIcon.MdRotateRight className="fs-3" /></button>
+                        <span className="tiptext">
+                            Girar a la derecha
+                        </span>
+                    </div>
+                </Col>
+            </Row>
+            {/* <div className="d-flex w-100 justify-content-end">
+
+            </div> */}
+
+        </Container>
     )
 }
 
