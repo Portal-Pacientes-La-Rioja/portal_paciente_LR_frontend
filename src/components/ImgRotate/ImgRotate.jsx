cf06f19b1662827a1e9fe3bb5451ac1e9788db6e
vpugliese <vpugliese@pall.cl>
2023-05-08 12:24:35 -0300

    Update: zoom image

--- src/components/ImgRotate/ImgRotate.jsx
+++ src/components/ImgRotate/ImgRotate.jsx
@@ -3,6 +3,7 @@ import notImg from '../../assets/statics/no-image-found.png';
 import * as MdIcon from 'react-icons/md';
 import { environment } from "../../environments/environments.demo";
 import { Col, Container, Row } from "react-bootstrap";
+import { useRef } from "react";
 
 const ImgRotate = ({ img }) => {
 
@@ -11,6 +12,8 @@ const ImgRotate = ({ img }) => {
     //rotate img
     const [rotate, setRotate] = useState(false);
     const [deg, setDeg] = useState(0);
+    const [zoom, setZoom] = useState(1);
+
     const rotateRigth = () => {
         setRotate(true)
         setDeg(deg + 90)
@@ -20,33 +23,56 @@ const ImgRotate = ({ img }) => {
         setDeg(deg - 90)
     }
 
+    const addZoom = () => {
+        let add = zoom
+        if (add++ < 5) setZoom(add++)
+    }
+    const removeZoom = () => {
+        let remove = zoom
+        if (remove-- > 1) setZoom(remove--)
+    }
     return (
         <Container className="mb-3 d-flex flex-column align-items-center">
             <Row>
                 <Col>
-                    <div className="admin-patient__img-container">
-                        <img className={`admin-patient__img ${rotate ? 'rotate-img' : ''}`} style={{ transform: `rotate(${deg}deg)` }} src={img ? img : nImg} alt={`document patient`} />
+                    <div className="admin-patient__img-container position-relative">
+                        <img className={`admin-patient__img`}
+                            style={{
+                                transform: `rotate(${deg}deg) scale(${zoom})`,
+                                position: 'absolute'
+                            }}
+                            src={img ? img : nImg}
+                            alt={`document patient`}
+                        />
                     </div>
                 </Col>
-                <Col className="d-flex">
-                    <div className="my-tooltip" style={{height: 'fit-content'}}>
+                <Col className="d-flex d-md-block">
+                    <div className="my-tooltip" style={{ height: 'fit-content' }}>
                         <button type="button" className='btn text-secondary btn-icon' onClick={(e) => rotateLeft()}><MdIcon.MdRotateLeft className="fs-3" /></button>
                         <span className="tiptext">
                             Girar a la izquierda
                         </span>
                     </div>
-                    <div className="my-tooltip" style={{height: 'fit-content'}}>
+                    <div className="my-tooltip" style={{ height: 'fit-content' }}>
                         <button type="button" className='btn text-secondary btn-icon' onClick={(e) => rotateRigth()}><MdIcon.MdRotateRight className="fs-3" /></button>
                         <span className="tiptext">
                             Girar a la derecha
                         </span>
                     </div>
+                    <div className="my-tooltip" style={{ height: 'fit-content' }}>
+                        <button type="button" className='btn text-secondary btn-icon' onClick={(e) => addZoom()}><MdIcon.MdOutlineZoomIn  className="fs-3"  /></button>
+                        <span className="tiptext">
+                            Zoom +
+                        </span>
+                    </div>
+                    <div className="my-tooltip" style={{ height: 'fit-content' }}>
+                        <button type="button" className='btn text-secondary btn-icon' onClick={(e) => removeZoom()}><MdIcon.MdOutlineZoomOut  className="fs-3"  /></button>
+                        <span className="tiptext">
+                            Zoom -
+                        </span>
+                    </div>
                 </Col>
             </Row>
-            {/* <div className="d-flex w-100 justify-content-end">
-
-            </div> */}
-
         </Container>
     )
 }
