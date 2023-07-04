ab319f1a1371bba45fd16ee39cb5d877b0e63d97
emeritus <alexsandradominguez0@gmail.com>
2023-07-04 11:24:47 -0300

    Se agregan las modificaciones en georeferencia y estudios

--- src/pages/Instituciones/pages/components/InstitutionMap/InstitutionMap.jsx
+++ src/pages/Instituciones/pages/components/InstitutionMap/InstitutionMap.jsx
@@ -1,19 +1,47 @@
 import { Col, Modal } from "react-bootstrap"
 import * as MdIcon from "react-icons/md";
 import MapView from "../../../../../components/MapsView/MapsView";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { useEffect } from "react";
 import Loader from "../../../../../components/Loader";
+import usePatient from "../../../../../hooks/usePatient"
+import { getShortestRoute } from "../../../../../services/institutionsServices";
 
 const InstitutionMap = ({ institution, show, handleClose }) => {
     const [loading, setLoading] = useState(true)
     const adress = institution.domicilio +', '+  institution.localidad +', '+ institution.departamento
+    const p = usePatient()
 
-    useEffect(() => {
+    const [ruta, setRuta] = useState(null);
+    useEffect(async () => {
         setTimeout(() => {
             setLoading(false)
         }, 500);
     }, [loading])
+
+    const rutaMasCorta = async () =>{
+        let patientId = p.patient.id;
+        let institucionId = institution.id;
+        let response;
+
+        try {
+            response = await getRutaMasCorta(patientId, institucionId);
+            setRuta(response);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const getRutaMasCorta = useCallback((patientId, institucionId)=>{
+        return getShortestRoute(patientId, institucionId)
+            .then((response) => {
+            return response;
+            })
+            .catch((error) => {
+            console.error(error);
+            throw error;
+            });
+    }, [])
 
     return (
         <Modal
@@ -30,7 +58,14 @@ const InstitutionMap = ({ institution, show, handleClose }) => {
                 ? <Loader isActive={true}/>
                 : <Modal.Body>
                     <Col xs={12} style={{height: '500px'}}>
-                        <MapView latitud={institution.lat ?? 0} longitud={institution.long ?? 0} descripcion={adress} />
+                        <button onClick={rutaMasCorta}>¿Cómo llego?</button>
+                        <MapView
+                            latitud={institution.lat ?? 0}
+                            longitud={institution.long ?? 0}
+                            descripcion={adress}
+                            coordinates={ruta}
+                        />
+                        {/* <MapView latitud={institution.lat ?? 0} longitud={institution.long ?? 0} descripcion={adress} /> */}
                     </Col>
                 </Modal.Body>
             }
