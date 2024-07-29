e525f4495fd9d20843f8442f13a2b24871850bc7
Violeta <violeta.pugliese@gmail.com>
2024-07-29 10:22:25 -0300

    Update: dependencies + config-override.js

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+const { override, addWebpackResolve } = require('customize-cra');
+
+module.exports = override(
+  addWebpackResolve({
+    fallback: {
+      "buffer": require.resolve("buffer/"),
+      "path": require.resolve("path-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "crypto": require.resolve("crypto-browserify"),
+      "util": require.resolve("util/"),
+      "stream": require.resolve("stream-browserify"),
+      "vm": require.resolve("vm-browserify"),
+      "process": require.resolve("process/browser"),
+      "zlib": require.resolve("browserify-zlib"),
+    }
+  })
+);
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './styles/index.scss';
 import 'bootstrap/scss/bootstrap.scss';
 import App from './App';
+import 'process/browser';
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
